7539da39cfe98874754688b1957aa91a62f5fe22
Nitesh Kumar <i.am.coder.nitesh@gmail.com>
2021-07-04 17:13:54 +0530

    Update and rename text.sh to launch.sh

--- launch.sh
+++ launch.sh
@@ -5,6 +5,7 @@ sudo apt update && sudo apt upgrade -y
 # Installing kitty & setting it up
 sudo apt install kitty -y
 sudo update-alternatives --config x-terminal-emulator
+mkdir $HOME/.config/kitty
 cp $HOME/Downloads/i3wm-ubuntu/config/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
 
 # Installing vim
