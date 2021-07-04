b1d9b10971d0fbac17d408fdf7a7083fab27c1da
Nitesh Kumar <i.am.coder.nitesh@gmail.com>
2021-07-04 16:35:36 +0530

    Update text.sh

--- text.sh
+++ text.sh
@@ -12,20 +12,20 @@ cp $HOME/Downloads/i3wm-ubuntu/config/kitty/kitty.conf $HOME/.config/kitty/kitty
 sudo apt install vim -y
 
 # ulauncher install
-sudo add-apt-repository ppa:agornostal/ulauncher
+sudo add-apt-repository ppa:agornostal/ulauncher -y
 sudo apt update -y
-sudo apt install ulauncher
+sudo apt install ulauncher -y
 
 # Installing required libraries
-sudo apt install libxcb-glx0 libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake libxcb-shape0-dev
+sudo apt install libxcb-glx0 libxcb-glx0-dev libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake libxcb-shape0-dev xcb-proto -y
 
 # SPEED RICER REPOSTIORY i3-gaps
-sudo add-apt-repository ppa:kgilmer/speed-ricer
-sudo apt update
-sudo apt install i3-gaps
+sudo add-apt-repository ppa:kgilmer/speed-ricer -y
+sudo apt update -y
+sudo apt install i3-gaps -y
 
 # Polybar dependencies & Install
-sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
 
 cd $HOME/Downloads
 git clone --recursive https://github.com/polybar/polybar
@@ -36,9 +36,9 @@ make -j$(nproc)
 sudo make install
 
 # Picom dependencies
-sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev
+sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev -y
 
-sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev
+sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev -y
 # Picom install
 cd $HOME/Downloads
 git clone https://github.com/ibhagwan/picom.git
@@ -49,7 +49,7 @@ ninja -C build
 sudo ninja -C build install
 
 # installing numlockx
-sudo apt install feh numlockx ttf-unifont
+sudo apt install feh numlockx ttf-unifont -y
 
 # Replacing files
 mkdir $HOME/Pictures/Wallpapers
@@ -59,5 +59,6 @@ mkdir $HOME/.config/polybar
 mkdir $HOME/.config/picom
 cp $HOME/Downloads/i3wm-ubuntu/config/polybar/config $HOME/.config/polybar/config
 cp $HOME/Downloads/i3wm-ubuntu/config/polybar/launch.sh $HOME/.config/polybar/launch.sh
-sudo chmod +x launch.sh && chmod +x launch.sh
+sudo chmod +x launch.sh 
+chmod +x launch.sh
 cp $HOME/Downloads/i3wm-ubuntu/config/picom/picom.conf $HOME/.config/picom/picom.conf
