245c0429f6c6f288fca15ca840309956f72b10ad
Nitesh Kumar <i.am.coder.nitesh@gmail.com>
2021-07-04 17:03:15 +0530

    Update text.sh

--- text.sh
+++ text.sh
@@ -24,9 +24,7 @@ sudo add-apt-repository ppa:kgilmer/speed-ricer -y
 sudo apt update -y
 sudo apt install i3-gaps -y
 
-# Polybar dependencies & Install
-sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
-
+# Polybar Install
 cd $HOME/Downloads
 git clone --recursive https://github.com/polybar/polybar
 cd $HOME/Downloads/polybar
@@ -35,10 +33,6 @@ cmake ..
 make -j$(nproc)
 sudo make install
 
-# Picom dependencies
-sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev -y
-
-sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev -y
 # Picom install
 cd $HOME/Downloads
 git clone https://github.com/ibhagwan/picom.git
