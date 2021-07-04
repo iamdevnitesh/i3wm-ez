092e964e77c246079a7d599a8a23e719c87fe77d
Nitesh Kumar <i.am.coder.nitesh@gmail.com>
2021-07-04 16:18:57 +0530

    Update text.sh

--- text.sh
+++ text.sh
@@ -17,7 +17,7 @@ sudo apt update -y
 sudo apt install ulauncher
 
 # Installing required libraries
-sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake libxcb-shape0-dev
+sudo apt install libxcb-glx0 libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake libxcb-shape0-dev
 
 # SPEED RICER REPOSTIORY i3-gaps
 sudo add-apt-repository ppa:kgilmer/speed-ricer
