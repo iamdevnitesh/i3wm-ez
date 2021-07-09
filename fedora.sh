c0af13843655920a7e9408aeef5047e5467b65e1
Nitesh Kumar <i.am.coder.nitesh@gmail.com>
2021-07-09 09:18:18 +0530

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -5,18 +5,18 @@ gsettings set org.gnome.desktop.default-applications.terminal exec kitty
 mkdir $HOME/.config/kitty
 
 # i3
-sudo dnf install i3-gaps
+sudo dnf install i3-gaps -y
 
 # POLYBAR
-sudo dnf install unifont-fonts
-sudo dnf install xorg-x11-apps
-sudo dnf install -y gcc-c++ clang git cmake @development-tools python3-sphinx python3-packaging
-sudo dnf install -y cairo-devel xcb-util-devel libxcb-devel xcb-proto xcb-util-image-devel xcb-util-wm-devel
-sudo dnf install -y xcb-util-xrm-devel xcb-util-cursor-devel alsa-lib-devel pulseaudio-libs-devel jsoncpp-devel libmpdclient-devel libcurl-devel wireless-tools-devel libnl3-devel
-sudo dnf install polybar
+sudo dnf install unifont-fonts -y
+sudo dnf install xorg-x11-apps -y
+sudo dnf install -y gcc-c++ clang git cmake @development-tools python3-sphinx python3-packaging -y
+sudo dnf install -y cairo-devel xcb-util-devel libxcb-devel xcb-proto xcb-util-image-devel xcb-util-wm-devel -y
+sudo dnf install -y xcb-util-xrm-devel xcb-util-cursor-devel alsa-lib-devel pulseaudio-libs-devel jsoncpp-devel libmpdclient-devel libcurl-devel wireless-tools-devel libnl3-devel -y
+sudo dnf install polybar -y
 
 # PICOM
-sudo dnf install -y dbus-devel gcc git libconfig-devel libdrm-devel libev-devel libX11-devel libX11-xcb libXext-devel libxcb-devel mesa-libGL-devel meson pcre-devel pixman-devel uthash-devel xcb-util-image-devel xcb-util-renderutil-devel xorg-x11-proto-devel
+sudo dnf install -y dbus-devel gcc git libconfig-devel libdrm-devel libev-devel libX11-devel libX11-xcb libXext-devel libxcb-devel mesa-libGL-devel meson pcre-devel pixman-devel uthash-devel xcb-util-image-devel xcb-util-renderutil-devel xorg-x11-proto-devel -y
 cd $HOME/Downloads
 git clone https://github.com/ibhagwan/picom.git
 cd picom
@@ -26,7 +26,7 @@ ninja -C build
 sudo ninja -C build install
 
 # FEH & NUMLOCKX
-sudo dnf install feh numlockx
+sudo dnf install feh numlockx -y
 
 # Replacing files
 mkdir $HOME/Pictures/Wallpapers
