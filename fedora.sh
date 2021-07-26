b114562ceb3d9b3e6a93f5e48338f141740d9835
Nitesh Kumar <i.am.coder.nitesh@gmail.com>
2021-07-26 21:20:09 +0530

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -5,9 +5,6 @@ gsettings set org.gnome.desktop.default-applications.terminal exec kitty
 mkdir $HOME/.config/kitty
 cp $HOME/Downloads/i3wm-ez/config/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
 
-# i3
-sudo dnf install i3-gaps -y
-
 # POLYBAR
 sudo dnf install unifont-fonts -y
 sudo dnf install xorg-x11-apps -y
@@ -15,6 +12,9 @@ sudo dnf install -y gcc-c++ clang git cmake @development-tools python3-sphinx py
 sudo dnf install -y cairo-devel xcb-util-devel libxcb-devel xcb-proto xcb-util-image-devel xcb-util-wm-devel
 sudo dnf install -y xcb-util-xrm-devel xcb-util-cursor-devel alsa-lib-devel pulseaudio-libs-devel jsoncpp-devel libmpdclient-devel libcurl-devel wireless-tools-devel libnl3-devel
 sudo dnf install polybar -y
+
+# i3
+sudo dnf install i3-gaps -y
 
 # PICOM
 sudo dnf install -y dbus-devel gcc git libconfig-devel libdrm-devel libev-devel libX11-devel libX11-xcb libXext-devel libxcb-devel mesa-libGL-devel meson pcre-devel pixman-devel uthash-devel xcb-util-image-devel xcb-util-renderutil-devel xorg-x11-proto-devel -y
